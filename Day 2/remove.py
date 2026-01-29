6b9b0c2e1abcfde6dbcdc480fc6635df2ea82ab4
Sarthak Katkar <sarthakkatkar14@gmail.com>
2026-01-29 19:36:31 +0530

    Rename student_info to stud_info in remove.py

--- Day 2/remove.py
+++ Day 2/remove.py
@@ -1,6 +1,6 @@
-student_info=[90,90,87,67]
-print(student_info)
+stud_info=[90,90,87,67]
+print(stud_info)
 
 data=int(input("\n Enter the data do you want to remove : "))
-student_info.remove(data)
-print(student_info)
+stud_info.remove(data)
+print(stud_info)
