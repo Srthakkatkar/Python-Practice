47332034ae27ddb61ca61bf048fc40ae46d6d8b2
Sarthak Katkar <sarthakkatkar14@gmail.com>
2026-01-30 14:59:18 +0530

    Add odd/even number checker in evenodd.py
    Implemented a program to check if a number is odd or even.

---
+++ Day 4/evenodd.py
@@ -0,0 +1,11 @@
+'''
+    Write a program to find the given number is odd or even 
+'''
+
+a=int(input("Enter the Number : "))
+
+if(a%2==0):
+    print("\n The   Number" ,a,"is even ")
+
+else:
+    print("\n The   Number is odd",a)
