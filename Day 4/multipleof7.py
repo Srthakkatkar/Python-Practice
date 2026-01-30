ee9abd9bf13dadbc8ef273481a99c5db0aedcdc1
Sarthak Katkar <sarthakkatkar14@gmail.com>
2026-01-30 14:57:57 +0530

    Add multipleof7.py to check multiples of 7
    Implement a program to check if a number is a multiple of 7.

---
+++ Day 4/multipleof7.py
@@ -0,0 +1,11 @@
+'''
+    write a program to check the number entered by user is multiple of 7 or not
+'''
+
+a=int(input("\n Enter the number A : "))
+
+if a%7==0:
+    print("\n The number is ",a,"Multiple of 7")
+
+else:
+    print("\n The number is ",a,"Not Multiple of 7")
