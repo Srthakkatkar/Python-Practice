2f5d81f17f5448dd468666cc8240904583ede440
Sarthak Katkar <sarthakkatkar14@gmail.com>
2026-02-01 17:41:03 +0530

    Add traffic signal response logic in traffic.py

---
+++ Day 6/traffic.py
@@ -0,0 +1,13 @@
+colour=input("Enter the Colur of Signal : ")
+
+if(colour=="Red" or colour=="red" or colour=="RED" ):
+    print(" STOP ")        
+
+elif(colour=="Yellow" or colour=="yellow" or colour=="YELLOW"):
+    print("\n Ready to GO")
+
+elif(colour=="Green" or colour=="green" or colour=="GREEN"):
+    print("\n GO ")
+
+else:
+    print("\n Invalid Information")
