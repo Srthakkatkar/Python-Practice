1d8bb62bab5d49c418d7b4be8a8eec71ec207312
Sarthak Katkar <sarthakkatkar14@gmail.com>
2026-02-04 19:59:55 +0530

    Add string replacement examples in str_replace.py

---
+++ Day 8/str_replace.py
@@ -0,0 +1,6 @@
+# str.replace()
+
+str1="Mahendra Singh Dhoni"
+str2="Suryakumar Yadav"
+print("Mahendra Singh Dhoni Changes to :",str1.replace("Mahendra Singh Dhoni","Dhoni"))
+print("Suryakumar Yadav Changes to : ",str2.replace("Suryakumar Yadav","Sky"))
