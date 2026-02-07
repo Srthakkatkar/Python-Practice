c7849abe54c3c033f6fc166cc70ae1bc089bd45a
Sarthak Katkar <sarthakkatkar14@gmail.com>
2026-02-07 17:36:01 +0530

    Implement student info modification in mutable.py
    Add functionality to modify student information in a list.

--- Day 11/mutable.py
+++ Day 11/mutable.py
@@ -1,1 +1,10 @@
+student_info=["Sarthak",96,"O"]
 
+print("\n The Lenght of List is : ",len(student_info))
+
+a=int(input("Enter Index do you want to change : "))
+str=input("\n Enter the info you want to change : ")
+
+student_info[a]=str
+
+print(student_info)
