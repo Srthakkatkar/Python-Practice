b920ec2aa37ca8be655f64af0890e28e3b1ca5d1
Sarthak Katkar <sarthakkatkar14@gmail.com>
2026-02-16 23:01:45 +0530

    Update and rename New Text Document - Copy.py to list_adv.py

---
+++ Day 17/list_adv.py
@@ -0,0 +1,15 @@
+numbers = [10, 20, 30, 20, 40, 10, 50]
+
+# Method 1: Using set()
+remove_set = list(set(numbers))
+
+# Method 2: Without changing order
+remove_order = []
+
+for num in numbers:
+    if num not in remove_order:
+        remove_order.append(num)
+
+print("Original List :", numbers)
+print("Using set() :", remove_set)
+print("Without changing order :", remove_order)
