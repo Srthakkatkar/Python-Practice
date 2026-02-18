c13c9c068d3d592c0aac47cc7fbba14d26b58c84
Sarthak Katkar <sarthakkatkar14@gmail.com>
2026-02-18 15:26:05 +0530

    Implement character search in string
    Add functionality to find all occurrences of a character in a string.

--- Day 18/str_find_adv.py
+++ Day 18/str_find_adv.py
@@ -1,1 +1,11 @@
+str1 = "Leo Messi"
+ch = input("Enter character to find: ")
+
+index = str1.find(ch)
+
+while index != -1:
+    print("Found at index:", index)
+    index = str1.find(ch, index + 1)
+
+
 
