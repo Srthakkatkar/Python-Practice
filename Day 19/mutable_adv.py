75c132dd84a1fbfd9d1d5ede2625155871126c2b
Sarthak Katkar <sarthakkatkar14@gmail.com>
2026-03-02 21:13:02 +0530

    Add shallow and deep copy examples in mutable_adv.py
    Demonstrate shallow and deep copy with a student info list.

--- Day 19/mutable_adv.py
+++ Day 19/mutable_adv.py
@@ -1,1 +1,25 @@
+import copy
+
+student_info = ["Atharv", 96, ["O"]]
+
+print("\nOriginal List :", student_info)
+
+# Shallow Copy
+shallow_copy = copy.copy(student_info)
+
+# Deep Copy
+deep_copy = copy.deepcopy(student_info)
+
+index = int(input("\nEnter index to change (grade list index = 2): "))
+value = input("Enter new value : ")
+
+# Changing nested list
+shallow_copy[index][0] = value
+deep_copy[index][0] = value
+
+print("\n After Modification ")
+
+print("\nOriginal List :", student_info)
+print("Shallow Copy  :", shallow_copy)
+print("Deep Copy     :", deep_copy)
 
